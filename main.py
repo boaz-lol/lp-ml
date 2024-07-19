d8504af6ce59a2752afe71e90d8ed93c57a595a4
황혜정 <hwanghyejeong@hwanghyejeong-ui-MacBookPro.local>
2024-07-19 11:25:42 +0900

    feat: kafka topic subscribe module

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
+from src.kafka import subscribe_topic
 from src.db import get_collection, get_db
 
 
 if __name__ == "__main__":
+
     # Get "data_source" collection
     data_source_collection =  get_collection("data_source")
-    
+
+    # Subscribe to "data_source" topic
+    subscribe_topic("data_source")
---
+++ src/kafka.py
@@ -0,0 +1,40 @@
+import os
+from typing import Dict, Any
+
+from confluent_kafka import Producer, Consumer, KafkaError, KafkaException
+
+
+KAFKA_BROKER_URL = os.getenv("KAFKA_BROKER_URL")
+
+def get_consumer() -> Consumer:
+    """
+    Creates and returns a Kafka consumer configured with the specified settings.
+
+    :return: Kafka consumer object
+    :raises KafkaException: If there is an error creating the Kafka consumer
+    """
+    try:
+        consumer_conf: Dict[str, Any] = {
+            'bootstrap.servers': KAFKA_BROKER_URL,
+            'group.id': 'my_group',
+            'auto.offset.reset': 'earliest'
+        }
+        consumer: Consumer = Consumer(**consumer_conf)
+        return consumer
+    except KafkaException as e:
+        print(f"Failed to create Kafka consumer: {e}")
+        raise
+
+def subscribe_topic(topic_name: str) -> None:
+    """
+    Subscribes the Kafka consumer to the specified topic.
+
+    :param topic_name: Name of the Kafka topic to subscribe to
+    :raises KafkaException: If there is an error subscribing to the topic
+    """
+    try:
+        consumer: Consumer = get_consumer()
+        consumer.subscribe([topic_name])
+    except KafkaException as e:
+        print(f"Failed to subscribe to topic {topic_name}: {e}")
+        raise
